8a6b4df7eaa8b655686b1d7972338097473b5d25
Moe Aboulkheir <moe@nervous.io>
2017-10-15 14:11:39 +0100

    Tidy sputter.test.util

--- test/sputter/test/util.clj
+++ test/sputter/test/util.clj
@@ -1,7 +1,6 @@
 (ns sputter.test.util
   (:require [sputter.util               :as util]
             [sputter.state              :as state]
-            [sputter.state.memory       :as mem]
             [sputter.state.storage      :as storage]
             [sputter.state.storage.stub :as storage.stub]
             [sputter.word               :as word]
@@ -9,12 +8,11 @@
             [cheshire.core              :as json]
             [clojure.java.io            :as io]
             [clojure.test               :as test :refer [is]]
-            [clojure.walk               :as walk]
             [clojure.string             :as str]))
 
 (def ^:private test-path "externals/ethereum-tests/VMTests")
 
-(defn hex-literal? [x]
+(defn- hex-literal? [x]
   (and (string? x) (str/starts-with? x "0x")))
 
 (let [renames {"gasPrice"          "gas-price"
@@ -23,15 +21,15 @@
                "currentNumber"     "number"
                "currentDifficulty" "difficulty"
                "currentTimestamp"  "timestamp"}]
-  (defn ->test-key [k]
+  (defn- ->test-key [k]
     (if (hex-literal? k)
       k
       (keyword (renames k k)))))
 
-(defn file->tests [f]
+(defn- file->tests [f]
   (-> f io/reader (json/parse-stream ->test-key)))
 
-(defn ->test-suite [& subpath]
+(defn- ->test-suite [& subpath]
   (let [path    (str test-path "/" (str/join "/" subpath))
         entries (->> path io/file file-seq)]
     (into []
@@ -40,9 +38,9 @@
        (mapcat file->tests))
       entries)))
 
-(def vm-tests (delay (->test-suite)))
+(def ^:private vm-tests (delay (->test-suite)))
 
-(defn named-tests [re]
+(defn- named-tests [re]
   (into []
     (for [[test-name test] @vm-tests
           :when (re-find re (name test-name))]
@@ -64,7 +62,7 @@
 (defn- map->storage [m]
   (storage.stub/->Storage (->storage-maps m)))
 
-(defn test->state [t]
+(defn- test->state [t]
   (let [exec (:exec t)]
     (state/map->State
      {:program (vm/disassemble (:code exec))
@@ -72,43 +70,32 @@
       :message {:recipient (word/->Word (:address exec))}
       :storage (map->storage (:pre t))})))
 
-(defn assert-gas [test exp state]
+(defn- assert-gas [test exp state]
   (when-let [exp (some-> exp hex->biginteger)]
     (is (= exp (:gas state))
         (str test ": Gas value mismatch. " exp " != " (:gas state)))))
 
 (let [zero (biginteger 0)]
-  (defn assert-return [test exp state]
+  (defn- assert-return [test exp state]
     (let [exp (hex->biginteger (or exp "0x"))
           act (:sputter/return state word/zero)]
       (is (= exp act)
           (str test ": Return value mismatch. " exp " != " act)))))
 
-(defn assert-error [test exp state]
+(defn- assert-error [test exp state]
   (if (:sputter/error state)
     (is (nil? (:gas exp))
         (str test ": Wants gas, but got error " (:sputter/error state)))
     (is (:gas exp) (str test ": No gas value?"))))
 
-(defn assert-mem [state exp]
-  (let [exp-storage (:storage exp)
-        mem         (:memory state)]
-    (doseq [[addr word] exp-storage
-            :let [addr (word/->Word addr)
-                  exp  (word/->Word word)
-                  act  (mem/recall mem addr)]]
-      (when (not= exp act)
-        (throw (ex-info (str "Memory value mismatch: " exp " != " act)
-                        {::vals [exp act]}))))))
-
-(defn assert-storage [test exp state]
+(defn- assert-storage [test exp state]
   (doseq [[addr pos->w] exp]
     (doseq [[pos w] pos->w :let [act-w (storage/retrieve state addr pos)]]
       (is (= w act-w)
           (str test ": Storage value mismatch for "
                addr ": " w " != " act-w)))))
 
-(defn run-vm-test [test-name test]
+(defn- run-vm-test [test-name test]
   (let [state (test->state test)
         post  (vm/execute state)]
     (assert-error   test-name test post)
@@ -116,14 +103,8 @@
     (assert-storage test-name (map->storage (:post test)) post)
     (assert-return  test-name (:out test) post)))
 
-(defonce last-error (atom nil))
-
 (defn run-vm-tests [re]
-  (try
-    (doseq [[test-name test] (named-tests re)]
-      (println "Running: " (name test-name))
-      (run-vm-test test-name test)
-      (is true))
-    (catch Exception e
-      (reset! last-error (or (ex-data e) e))
-      (throw e))))
+  (doseq [[test-name test] (named-tests re)]
+    (println "Running: " (name test-name))
+    (run-vm-test test-name test)
+    (is true)))
